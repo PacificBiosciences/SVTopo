221df40cb85d1490d25cfbdcfa0827863e2f8acb
Jon Belyeu <jbelyeu@pacificbiosciences.com>
2024-10-22 16:04:29 -0700

    add suite of manual tests to verify installation

---
+++ test/scripts/compare_files.py
@@ -0,0 +1,129 @@
+from pprint import pprint
+from skimage.metrics import structural_similarity as ssim
+from skimage import io
+import argparse
+import sys
+import json
+
+MATCHING_SSI = 0.98
+
+
+class Colors:
+    RED = "\033[91m"
+    GREEN = "\033[92m"
+    RESET = "\033[0m"
+
+
+def setup():
+    parser = argparse.ArgumentParser(
+        description="Compare two images to determine if essentially identical",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "-r",
+        "--reference-file-path",
+        help="the path to a reference file, either json or image",
+    )
+    parser.add_argument(
+        "-t",
+        "--test-file-path",
+        help="the path to a test file, either json or image",
+    )
+    args = parser.parse_args()
+    return args
+
+
+def compare_plots(reference_image_path, test_image_path):
+    """
+    Compare two images and report an error if they're dissimilar
+    """
+    test_image = io.imread(test_image_path)
+    reference_image = io.imread(reference_image_path)
+    # window size has to be equal to or less than the smallest side of the input images
+    win_size = int(min(test_image.shape[:2] + reference_image.shape[:2]))
+    # must be odd
+    if win_size % 2 == 0:
+        win_size -= 1
+    similarity_index, _ = ssim(
+        test_image, reference_image, full=True, win_size=win_size, channel_axis=-1
+    )
+    print("SSI:", similarity_index)
+    if similarity_index < MATCHING_SSI:
+        print(
+            "{}Test image {} does not match reference image {}: SSI={}{}".format(
+                Colors.RED,
+                test_image_path,
+                reference_image_path,
+                similarity_index,
+                Colors.RESET,
+            ),
+            file=sys.stderr,
+        )
+        sys.exit(-1)
+    else:
+        print(
+            "{}Test image {} matches reference image {}: SSI={}{}".format(
+                Colors.GREEN,
+                test_image_path,
+                reference_image_path,
+                similarity_index,
+                Colors.RESET,
+            ),
+            file=sys.stdout,
+        )
+
+
+def ordered(obj):
+    if isinstance(obj, dict):
+        return sorted((k, ordered(v)) for k, v in obj.items())
+    if isinstance(obj, list):
+        return sorted(ordered(x) for x in obj)
+    else:
+        return obj
+
+
+def compare_jsons(reference_json_path, test_json_path):
+    """
+    Compare two json files from cnidaria and error if they don't match
+    """
+    with open(reference_json_path, "r") as ref_fh:
+        ref_json = json.load(ref_fh)
+    with open(test_json_path, "r") as test_fh:
+        try:
+            test_json = json.load(test_fh)
+        except json.decoder.JSONDecodeError:
+            print(
+                "{}Test json {} is empty or malformed {}".format(
+                    Colors.RED, test_file_path, Colors.RESET
+                ),
+                file=sys.stderr,
+            )
+            sys.exit(-1)
+    if not ordered(ref_json) == ordered(test_json):
+        with open("tmp1", "w") as fh:
+            pprint(ordered(ref_json), stream=fh)
+        with open("tmp2", "w") as fh:
+            pprint(ordered(test_json), stream=fh)
+        print(
+            "{}Test json {} does not match reference json {}{}".format(
+                Colors.RED, test_file_path, reference_json_path, Colors.RESET
+            ),
+            file=sys.stderr,
+        )
+        sys.exit(-1)
+    else:
+        print(
+            "{}Test json {} matches reference json {}{}".format(
+                Colors.GREEN, test_file_path, reference_json_path, Colors.RESET
+            ),
+            file=sys.stdout,
+        )
+
+
+args = setup()
+ref_file_path = args.reference_file_path
+test_file_path = args.test_file_path
+if ref_file_path.endswith("json"):
+    compare_jsons(reference_json_path=ref_file_path, test_json_path=test_file_path)
+else:
+    compare_plots(reference_image_path=ref_file_path, test_image_path=test_file_path)
---
+++ test/scripts/run_end_to_end_tests.sh
@@ -0,0 +1,88 @@
+set -e
+svtopo=$1
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+final_color=$GREEN
+
+
+if [ "$svtopo" = "" ];
+then
+    echo "Missing svtopo binary"
+    exit 1
+fi
+
+vcf=test/data/vcf/sawfish.vcf.gz
+reads_json=test/data/json/supporting_reads.json.gz
+wget https://github.com/PacificBiosciences/HiFiCNV/raw/refs/heads/main/data/excluded_regions/cnv.excluded_regions.hg38.bed.gz -O cnv.excluded_regions.hg38.bed.gz
+hg38=cnv.excluded_regions.hg38.bed.gz
+
+for bam in test/data/bam/*bam;
+do
+    echo $bam
+    echo ""
+
+    chrom=$(basename $bam | cut -f 1 -d '_')
+    start=$(basename $bam | cut -f 2 -d '_')
+    end=$(basename $bam | cut -f 3 -d '_' | cut -f 1 -d '.')
+    runtype=$(basename $bam | cut -f 4 -d '_' | cut -f 1 -d '.')
+
+    expected_image=$(ls test/data/image/"$chrom"-"$start"-"$end"*.png)
+    expected_json="test/data/json/"$chrom"_"$start"_"$end".json"
+
+    # run svtopo
+    tmpname="test"
+    test_json_name=${tmpname}_${chrom}_${start}.json
+    if [ "$runtype" = "vcf" ];
+    then
+        $svtopo \
+            --bam $bam \
+            --verbose \
+            --variant-readnames $reads_json \
+            --vcf $vcf \
+            --exclude-regions $hg38 \
+            > $test_json_name \
+            2> svtopo_tests.log
+    else
+        $svtopo \
+            --bam $bam \
+            --verbose \
+            --exclude-regions $hg38 \
+            > $test_json_name \
+            2> svtopo_tests.log
+    fi
+    svtopovz \
+        --json $test_json_name \
+        --out-prefix $tmpname\
+        --include-simple-dups\
+        --include-simple-dels\
+        --verbose 2>> svtopo_tests.log
+
+    if [ $? -ne 0 ]; then 
+        final_color="$RED"
+    fi
+    
+    # compare results against saved files
+    python test/scripts/compare_files.py -r $expected_json -t $test_json_name
+    if [ $? -ne 0 ]; then 
+        final_color="$RED"
+    fi
+
+    #test image output from svtopovz
+    for test_image in ${tmpname}*.png;
+    do
+        test_image=$(ls $test_image)
+        if [ $? -ne 0 ]; then 
+            final_color="$RED"
+        fi
+        python test/scripts/compare_files.py -r $expected_image -t $test_image
+        if [ $? -ne 0 ]; then 
+            final_color="$RED"
+        fi
+        echo ""
+    done
+    rm ${tmpname}*png
+    rm $test_json_name
+    rm svtopo_tests.log
+done
+
+printf "\n${final_color}Tests finished\n"
